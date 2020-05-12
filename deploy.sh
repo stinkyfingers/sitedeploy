6749151c73731fa3d97899fe1c29c9f73f9a609e
John Shenk <john_shenk@hotmail.com>
2020-05-12 14:52:44 -0500

    first commit

---
+++ deploy.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+
+FULL_PATH=$1;
+PROJECT="$(basename "$FULL_PATH")";
+DIRNAME="$(dirname "$FULL_PATH")";
+SITEDEPLOYDIR=$(pwd);
+AWS_PROFILE='jds';
+AWS_REGION='us-west-1';
+
+validate () {
+  echo 'VALIDATING';
+  usage="USAGE:\n\t./deploy <dir/project_name>\n\tYou must specify a directory and project name.";
+
+  # check for project path/name
+  if [ -z $FULL_PATH ]; then echo $usage; exit 1; fi;
+  if [ -z $DIRNAME ] || [[ $DIRNAME = '.' ]] ; then echo $usage; exit 1; fi;
+  if [ -z $PROJECT ]; then echo $usage; exit 1; fi;
+  if [[ ! -d $DIRNAME ]]; then echo "path $DIRNAME does not exist"; exit 1; fi;
+
+  # repo
+  STATUS_CODE=$(curl https://github.com/stinkyfingers/$BASE -I -s -w "%{http_code}" -o /dev/null);
+  if [ $STATUS_CODE -ne 200 ]; then echo 'Repository does not exist'; exit 1; fi;
+
+  echo 'building project at:' $FULL_PATH;
+}
+
+tf () {
+  echo 'TERRAFORM';
+  cd $FULL_PATH;
+  echo 'here', $PWD
+  echo "terraform/.terraform/*\nterraform/terraform.tfstate*" >> .gitignore;
+
+  rm -rf $FULL_PATH/terraform; # TODO remove
+  cp -r $SITEDEPLOYDIR/terraform terraform;
+
+  echo "
+  project=\"$PROJECT\"
+  " > .tfvars;
+
+  echo "terraform {
+    backend \"s3\" {
+      bucket = \"remotebackend\"
+      key    = \"${PROJECT}/terraform.tfstate\"
+      region = \"${AWS_REGION}\"
+      profile = \"${AWS_PROFILE}\"
+    }
+  }" > terraform/backend.tf;
+
+  cd terraform;
+  terraform init;
+  terraform apply -auto-approve -var="project=$PROJECT" -var="region=$AWS_REGION";
+}
+
+react_app () {
+  echo 'REACTJS APP';
+  cd $DIRNAME;
+  npx create-react-app $PROJECT;
+}
+
+buildspec () {
+  echo 'CI';
+
+  cd $FULL_PATH;
+  # get distribution id
+  DISTRIBUTION_ID=$(aws cloudfront list-distributions --profile $AWS_PROFILE | jq -r ".DistributionList.Items[] | select(.DefaultCacheBehavior.TargetOriginId==\"$PROJECT-origin\") | .Id");
+  echo "version: 0.2
+phases:
+  install:
+    runtime-versions:
+      nodejs: 10
+  pre_build:
+    commands:
+      - yarn
+  build:
+    commands:
+      - yarn build
+  post_build:
+    commands:
+      - aws s3 sync build s3://$PROJECT.john-shenk.com
+      - aws cloudfront create-invalidation --paths /index.html --distribution-id $DISTRIBUTION_ID
+artifacts:
+  files:
+    - 'build/*'
+" > buildspec.yml;
+}
+
+version_control () {
+  echo 'VERSION CONTROL';
+  cd $FULL_PATH;
+  git init;
+  git add -A;
+  git commit -am "first commmit";
+  git remote add origin https://github.com/stinkyfingers/$PROJECT.git;
+  git push -u origin master;
+}
+
+validate;
+react_app;
+tf;
+buildspec;
+version_control;
---
+++ terraform/main.tf
@@ -0,0 +1,108 @@
+# provider
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
+# s3
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.project}.${var.domain}"
+  acl = "private"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "Cloudfront Read",
+          "Effect": "Allow",
+          "Principal": {
+              "AWS": "${aws_cloudfront_origin_access_identity.oai.iam_arn}"
+          },
+          "Action": "s3:GetObject",
+          "Resource": [
+            "arn:aws:s3:::${var.project}.${var.domain}/*",
+            "arn:aws:s3:::${var.project}.${var.domain}"
+          ]
+      }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket_public_access_block" "pab" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "${var.project}.${var.domain} identity"
+}
+
+locals {
+  s3_origin_id = "${var.project}-origin"
+}
+
+resource "aws_cloudfront_distribution" "distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_domain_name
+    origin_id   = local.s3_origin_id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = ["${var.project}.${var.domain}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+    trusted_signers = []
+
+    forwarded_values {
+      query_string = false
+      query_string_cache_keys = []
+      headers = []
+
+      cookies {
+        forward = "none"
+        whitelisted_names = []
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 31536000
+  }
+  restrictions {
+    geo_restriction {
+        locations        = []
+        restriction_type = "none"
+    }
+}
+
+  viewer_certificate {
+      acm_certificate_arn            = var.certificate_arn
+      cloudfront_default_certificate = false
+      minimum_protocol_version       = "TLSv1.1_2016"
+      ssl_support_method             = "sni-only"
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.project}.${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
